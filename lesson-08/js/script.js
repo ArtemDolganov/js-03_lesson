66a655a397b0df37d160f974f7e2a5d2c680bbe5
ArtemDolganov <280779@list.ru>
2018-08-22 23:16:50 +0300

    Commit 12

--- lesson-08/js/script.js
+++ lesson-08/js/script.js
@@ -49,8 +49,10 @@ window.addEventListener('DOMContentLoaded', function() {
 		    minutes = Math.floor( (t / 1000 / 60) % 60),
 		    seconds = Math.floor( (t / 1000) % 60);
 
-		if (hours < 10) hours = '0'+ hours;
-		
+		if (hours <10) hours = '0'+ hours;
+		if (minutes < 10) minutes  = "0" + minutes;
+    	if (seconds < 10) seconds  = "0" + seconds;
+    	    	
 		if (t <= 0){
 		return {
 	    'total' 	: 0,
@@ -74,6 +76,7 @@ window.addEventListener('DOMContentLoaded', function() {
 		hours = timer.querySelector('.hours'),
 		minutes = timer.querySelector('.minutes'),
 		seconds = timer.querySelector('.seconds');
+
 		let timeInterval = setInterval(updateClock, 1000);
 
 
