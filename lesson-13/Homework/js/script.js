d44ea1dc03b1b79286d201367d0b3e8cd37a1d48
ArtemDolganov <280779@list.ru>
2018-08-31 19:31:29 +0300

    Commit 25

---
+++ lesson-13/Homework/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('.main_btna').click(function() {
+        $('.modal').slideDown(1000)
+        $('.overlay').fadeIn(1000)
+    });
+
+    $('.main_btn').click(function() {
+        $('.modal').slideDown(1000)
+        $('.overlay').fadeIn(1000)
+    });
+
+    $('a[href="#sheldure"]').click(function() {
+        $('.modal').slideDown(1000)
+        $('.overlay').fadeIn(1000)
+    });
+
+    
+    $('span').click(function() {
+        $('.modal').slideUp(1000)
+        $('.overlay').fadeOut(1000)
+    })
+    
+});
